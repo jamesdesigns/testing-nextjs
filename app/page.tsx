fac568d705a5851a5687b00a99dd0ec467e57719
jamesdesigns <jameshooperdesigns@yahoo.com>
2023-11-17 14:44:01 -0700

    Add asea links to navigation

--- app/page.tsx
+++ app/page.tsx
@@ -3,56 +3,35 @@ import Image from 'next/image'
 export default function Home() {
   return (
     
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-6">
 
-
-
-
-<ul className="flex">
-  <li className="mr-6">
-    <a className="text-black-500 hover:text-blue-800" href="#">Active</a>
-  </li>
-  <li className="mr-6">
-    <a className="text-black-500 hover:text-blue-800" href="#">Link</a>
-  </li>
-  <li className="mr-6">
-    <a className="text-black-500 hover:text-blue-800" href="#">Link</a>
-  </li>
-  <li className="mr-6">
-    <a className="text-gray-400 cursor-not-allowed" href="#">Disabled</a>
-  </li>
-</ul>
-
-
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-
-
-
-
-        
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      <ul className="w-24 min-w-full flex justify-between">
+        <div className="flex-row">
+          <li>ASEA</li>
         </div>
-      </div>
+        <div className="flex flex-row">
+          <li className="mr-6">
+            <a className="text-black-500 hover:text-blue-800" href="#">Products</a>
+          </li>
+          <li className="mr-6">
+            <a className="text-black-500 hover:text-blue-800" href="#">Opportunity</a>
+          </li>
+          <li className="mr-6">
+            <a className="text-black-500 hover:text-blue-800" href="#">Science</a>
+          </li>
+          <li className="mr-6">
+            <a className="text-black-500 hover:text-blue-800" href="#">Company</a>
+          </li>
+          <li className="mr-6">
+            <a className="text-black-500 hover:text-blue-800" href="#">Discover</a>
+          </li>
+        </div>
+        <div className="flex flex-row">
+          <li className="mr-6"><a className="" href="#">Search</a></li>
+          <li className="mr-6"><a className="" href="#">Log In</a></li>
+          <li className="mr-6"><a className="" href="#">EN</a></li>
+        </div>
+      </ul>
 
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
         {/* <Image
