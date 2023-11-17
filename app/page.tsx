3738bc169279065f992a5324f029c90535f4c461
jamesdesigns <jameshooperdesigns@yahoo.com>
2023-11-17 15:55:25 -0700

    Add Breadcrumbs to the top of the footer

--- app/page.tsx
+++ app/page.tsx
@@ -64,93 +64,31 @@ export default function Home() {
       </div>
 
 
-      <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
-        <div className="md:flex">
-          <div className="md:shrink-0">
-            <img className="h-48 w-full object-cover md:h-full md:w-48" src="https://aseaglobal.com/wp-content/uploads/2023/02/RENU28-Hero-1-720x720-1.jpg" alt="Modern building architecture" />
-          </div>
-          <div className="p-8">
-            <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Company retreats</div>
-            <a href="#" className="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Incredible accommodation for your team</a>
-            <p className="mt-2 text-slate-500">Looking to take your team away on a retreat to enjoy awesome food and take in some sunshine? We have a list of places to do just that.</p>
-          </div>
-        </div>
-      </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-
-      <footer className="bg-neutral-100 text-center text-neutral-600 dark:bg-neutral-600 dark:text-neutral-200 lg:text-left">
+      <footer className="bg-neutral-100 text-center text-neutral-600 dark:bg-neutral-600 dark:text-neutral-200 lg:text-left pl-10 pr-10">
         <div className="flex items-center justify-center border-b-2 border-neutral-200 p-6 dark:border-neutral-500 lg:justify-between">
-        <div className="mr-12 hidden lg:block">
-          <span>Get connected with us on social networks:</span>
+        <div className="flex flex-row">
+          <span>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 26 26" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+            </svg>
+          </span>
+          <span>&nbsp;</span>
+          <span>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+            </svg>
+          </span>
+          <span>&nbsp;</span>
+          <span>ASEA</span>
+          <span>&nbsp;</span>
+          <span>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+            </svg>
+          </span>
+          <span>&nbsp;</span>
+          <span>Products</span>
         </div>
 
         <div className="flex justify-center">
@@ -219,12 +157,12 @@ export default function Home() {
         </div>
         </div>
 
-        <div className="mx-6 py-10 text-center md:text-left">
-          <div className="grid-1 grid gap-8 md:grid-cols-2 lg:grid-cols-4">
+        <div className="mx-5 py-10 text-center md:text-left">
+          <div className="grid-1 grid gap-8 md:grid-cols-2 lg:grid-cols-5">
 
             <div className="">
               <h6
-                className="mb-4 flex items-center justify-center font-semibold uppercase md:justify-start">
+                className="mb-5 flex items-center justify-center font-semibold uppercase md:justify-start">
                 {/* <svg
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 24 24"
@@ -244,105 +182,103 @@ export default function Home() {
 
             <div className="">
               <h6
-                className="mb-4 flex justify-center font-semibold uppercase md:justify-start">
+                className="mb-5 flex items-center justify-center font-semibold uppercase md:justify-start">
                 Products
               </h6>
-              <p className="mb-4">
-                <a href="#!" className="text-neutral-600 dark:text-neutral-200">Angular</a>
+              <p className="mb-5">
+                <a href="#!" className="text-neutral-600 dark:text-neutral-200">ASEA Redox Supplement</a>
               </p>
-              <p className="mb-4">
-                <a href="#!" className="text-neutral-600 dark:text-neutral-200">React</a>
+              <p className="mb-5">
+                <a href="#!" className="text-neutral-600 dark:text-neutral-200">RENU28</a>
               </p>
-              <p className="mb-4">
-                <a href="#!" className="text-neutral-600 dark:text-neutral-200">Vue</a>
+              <p className="mb-5">
+                <a href="#!" className="text-neutral-600 dark:text-neutral-200">RENUAdvanced</a>
               </p>
-              <p>
-                <a href="#!" className="text-neutral-600 dark:text-neutral-200">Laravel</a>
+              <p className="mb-5">
+                <a href="#!" className="text-neutral-600 dark:text-neutral-200">ASEA Redox Clay Mask</a>
+              </p>
+              <p className="mb-5">
+                <a href="#!" className="text-neutral-600 dark:text-neutral-200">Cell Performance</a>
+              </p>
+              <p className="mb-5">
+                <a href="#!" className="text-neutral-600 dark:text-neutral-200">ASEA VIA</a>
               </p>
             </div>
 
             <div className="">
               <h6
-                className="mb-4 flex justify-center font-semibold uppercase md:justify-start">
-                Useful links
+                className="mb-5 flex justify-center font-semibold uppercase md:justify-start">
+                Opportunity
               </h6>
-              <p className="mb-4">
-                <a href="#!" className="text-neutral-600 dark:text-neutral-200">Pricing</a>
+              <p className="mb-5">
+                <a href="#!" className="text-neutral-600 dark:text-neutral-200">Earn</a>
               </p>
-              <p className="mb-4">
-                <a href="#!" className="text-neutral-600 dark:text-neutral-200">Settings</a>
+              <p className="mb-5">
+                <a href="#!" className="text-neutral-600 dark:text-neutral-200">Events</a>
               </p>
-              <p className="mb-4">
-                <a href="#!" className="text-neutral-600 dark:text-neutral-200">Orders</a>
+            </div>
+
+            <div className="">
+              <h6
+                className="mb-5 flex justify-center font-semibold uppercase md:justify-start">
+                Science
+              </h6>
+              <p className="mb-5">
+                <a href="#!" className="text-neutral-600 dark:text-neutral-200">Science</a>
               </p>
-              <p>
-                <a href="#!" className="text-neutral-600 dark:text-neutral-200">Help</a>
+              <p className="mb-5">
+                <a href="#!" className="text-neutral-600 dark:text-neutral-200">Science</a>
+              </p>
+              <p className="mb-5">
+                <a href="#!" className="text-neutral-600 dark:text-neutral-200">Science</a>
+              </p>
+              <p className="mb-5">
+                <a href="#!" className="text-neutral-600 dark:text-neutral-200">Science</a>
+              </p>
+            </div>
+
+            <div className="">
+              <h6
+                className="mb-5 flex justify-center font-semibold uppercase md:justify-start">
+                Company
+              </h6>
+              <p className="mb-5">
+                <a href="#!" className="text-neutral-600 dark:text-neutral-200">Contact us</a>
+              </p>
+              <p className="mb-5">
+                <a href="#!" className="text-neutral-600 dark:text-neutral-200">About us</a>
+              </p>
+              <p className="mb-5">
+                <a href="#!" className="text-neutral-600 dark:text-neutral-200">Leadership</a>
+              </p>
+              <p className="mb-5">
+                <a href="#!" className="text-neutral-600 dark:text-neutral-200">Careers</a>
+              </p>
+              <p className="mb-5">
+                <a href="#!" className="text-neutral-600 dark:text-neutral-200">Mission Vision Values</a>
+              </p>
+              <p className="mb-5">
+                <a href="#!" className="text-neutral-600 dark:text-neutral-200">Advisory Council</a>
               </p>
             </div>
 
             <div>
               <h6
-                className="mb-4 flex justify-center font-semibold uppercase md:justify-start">
-                Contact
+                className="mb-5 flex justify-center font-semibold uppercase md:justify-start">
+                Discover
               </h6>
-              <p className="mb-4 flex items-center justify-center md:justify-start">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                  className="mr-3 h-5 w-5">
-                  <path
-                    d="M11.47 3.84a.75.75 0 011.06 0l8.69 8.69a.75.75 0 101.06-1.06l-8.689-8.69a2.25 2.25 0 00-3.182 0l-8.69 8.69a.75.75 0 001.061 1.06l8.69-8.69z" />
-                  <path
-                    d="M12 5.432l8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 01-.75-.75v-4.5a.75.75 0 00-.75-.75h-3a.75.75 0 00-.75.75V21a.75.75 0 01-.75.75H5.625a1.875 1.875 0 01-1.875-1.875v-6.198a2.29 2.29 0 00.091-.086L12 5.43z" />
-                </svg>
-                New York, NY 10012, US
+              <p className="mb-5 flex items-center justify-center md:justify-start">
+                Press release
               </p>
-              <p className="mb-4 flex items-center justify-center md:justify-start">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                  className="mr-3 h-5 w-5">
-                  <path
-                    d="M1.5 8.67v8.58a3 3 0 003 3h15a3 3 0 003-3V8.67l-8.928 5.493a3 3 0 01-3.144 0L1.5 8.67z" />
-                  <path
-                    d="M22.5 6.908V6.75a3 3 0 00-3-3h-15a3 3 0 00-3 3v.158l9.714 5.978a1.5 1.5 0 001.572 0L22.5 6.908z" />
-                </svg>
-                info@example.com
-              </p>
-              <p className="mb-4 flex items-center justify-center md:justify-start">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                  className="mr-3 h-5 w-5">
-                  <path
-                    fill-rule="evenodd"
-                    d="M1.5 4.5a3 3 0 013-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 01-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 006.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 011.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 01-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5z"
-                    clip-rule="evenodd" />
-                </svg>
-                + 01 234 567 88
-              </p>
-              <p className="flex items-center justify-center md:justify-start">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                  className="mr-3 h-5 w-5">
-                  <path
-                    fill-rule="evenodd"
-                    d="M7.875 1.5C6.839 1.5 6 2.34 6 3.375v2.99c-.426.053-.851.11-1.274.174-1.454.218-2.476 1.483-2.476 2.917v6.294a3 3 0 003 3h.27l-.155 1.705A1.875 1.875 0 007.232 22.5h9.536a1.875 1.875 0 001.867-2.045l-.155-1.705h.27a3 3 0 003-3V9.456c0-1.434-1.022-2.7-2.476-2.917A48.716 48.716 0 0018 6.366V3.375c0-1.036-.84-1.875-1.875-1.875h-8.25zM16.5 6.205v-2.83A.375.375 0 0016.125 3h-8.25a.375.375 0 00-.375.375v2.83a49.353 49.353 0 019 0zm-.217 8.265c.178.018.317.16.333.337l.526 5.784a.375.375 0 01-.374.409H7.232a.375.375 0 01-.374-.409l.526-5.784a.373.373 0 01.333-.337 41.741 41.741 0 018.566 0zm.967-3.97a.75.75 0 01.75-.75h.008a.75.75 0 01.75.75v.008a.75.75 0 01-.75.75H18a.75.75 0 01-.75-.75V10.5zM15 9.75a.75.75 0 00-.75.75v.008c0 .414.336.75.75.75h.008a.75.75 0 00.75-.75V10.5a.75.75 0 00-.75-.75H15z"
-                    clip-rule="evenodd" />
-                </svg>
-                + 01 234 567 89
+              <p className="mb-5 flex items-center justify-center md:justify-start">
+                Media mention
               </p>
             </div>
           </div>
         </div>
 
 
-        <div className="bg-neutral-200 p-6 text-center dark:bg-neutral-700">
+        <div className="bg-neutral-200 p-5 text-center dark:bg-neutral-700">
           <span>© 2023 Copyright:</span>
           <a className="font-semibold text-neutral-600 dark:text-neutral-400" href="https://tw-elements.com/">TW elements</a>
         </div>
