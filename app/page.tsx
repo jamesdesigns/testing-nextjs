5140c92a5a6bd72624df4010a83d31323b91d3f0
jamesdesigns <jameshooperdesigns@yahoo.com>
2023-11-20 10:28:35 -0700

    Edit the hover color to cyan from blue

--- app/page.tsx
+++ app/page.tsx
@@ -18,37 +18,37 @@ export default function Home() {
         </div>
         <div className="flex flex-row items-center">
           <li className="mr-6 px-8">
-            <a className="text-gray-500 hover:text-blue-800" href="#">Products</a>
+            <a className="text-gray-500 hover:text-cyan-800" href="#">Products</a>
           </li>
           <li className="mr-6 px-8">
-            <a className="text-gray-500 hover:text-blue-800" href="#">Opportunity</a>
+            <a className="text-gray-500 hover:text-cyan-800" href="#">Opportunity</a>
           </li>
           <li className="mr-6 px-8">
-            <a className="text-gray-500 hover:text-blue-800" href="#">Science</a>
+            <a className="text-gray-500 hover:text-cyan-800" href="#">Science</a>
           </li>
           <li className="mr-6 px-8">
-            <a className="text-gray-500 hover:text-blue-800" href="#">Company</a>
+            <a className="text-gray-500 hover:text-cyan-800" href="#">Company</a>
           </li>
           <li className="mr-6 px-8">
-            <a className="text-gray-500 hover:text-blue-800" href="#">Discover</a>
+            <a className="text-gray-500 hover:text-cyan-800" href="#">Discover</a>
           </li>
         </div>
         <div className="flex flex-row items-center pr-10">
           <li className="mr-6">
-            <a className="text-black-500 hover:text-blue-800" href="#">
+            <a className="text-black-500 hover:text-cyan-800" href="#">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 26 26" stroke-width="2" stroke="currentColor" className="w-6 h-6">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
               </svg>
             </a>
           </li>
           <li className="mr-6">
-            <a className="text-black-500 hover:text-blue-800" href="#">
+            <a className="text-black-500 hover:text-cyan-800" href="#">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 26 26" stroke-width="2" stroke="currentColor" className="w-6 h-6">
               <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
             </svg>
             </a>
           </li>
-          <li className="mr-6"><a className="text-gray-500 hover:text-blue-800" href="#">EN</a></li>
+          <li className="mr-6"><a className="text-gray-500 hover:text-cyan-800" href="#">EN</a></li>
         </div>
       </ul>
 
