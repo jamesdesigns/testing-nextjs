b121687fd0744e14275ca8ea40492e2d5c106b90
jamesdesigns <jameshooperdesigns@yahoo.com>
2023-11-21 09:52:48 -0700

    Make minor adjustments to the top right navigation

--- app/page.tsx
+++ app/page.tsx
@@ -34,7 +34,7 @@ export default function Home() {
           </li>
         </div>
         <div className="flex flex-row items-center font-light pr-10">
-          <li className="mr-6">
+          <li className="mr-5">
             <a className="text-zinc-700 hover:text-sky-600" href="#">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 26 26" stroke-width="2" stroke="currentColor" className="w-6 h-6 ml-3">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
@@ -69,9 +69,6 @@ export default function Home() {
 
         <div className="flex items-start flex-row font-light content-start z-[10]">
             <span><a href="#!" className="text-zinc-500 hover:text-sky-600">
-              {/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 26 26" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
-              </svg> */}
               <img src="icons8-home.svg" width="24" height="auto" />
               </a>
             </span>
